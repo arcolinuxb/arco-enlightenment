8c838566ee1cbd34e338bf382d5d48bd7cc6e614
arcolinuxz <arcolinuxinfo@gmail.com>
2023-06-30 06:26:56 +0200

    bump nr

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="light"
 	dmDesktop="enlightenment"
 
-	arcolinuxVersion='v23.07.01'
+	arcolinuxVersion='v23.09.01'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
